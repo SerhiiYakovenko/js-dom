1e0313ff3176c672d4ddfbb0d47eaf457e698b42
Serhii Yakovenko <serhii.yakovenko@toptal.com>
2020-12-28 21:26:12 +0200

    finishing task

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 const items = [
     {
         heading: 'City at day',
-        img: '.img/city.jpg',
+        img: 'img/city.jpg',
         text: 'Enter the massive open world of Night City, a place that sets new standards in terms of visuals, complexity and depth.'
     },
     {
         heading: 'City at night',
-        img: '.img/night-city.jpg',
+        img: 'img/night-city.jpg',
         text: `Cyberpunk 2077 is an open-world, action-adventure story set in Night City, a megalopolis obsessed with power, glamour and body modification.
         You play as V, a mercenary outlaw going after a one-of-a-kind implant that is the key to immortality.
         You can customize your character’s cyberware, skillset and playstyle, and explore a vast city where the choices you make shape the story and the world around you.`
     },
     {
         heading: 'Mercenary',
-        img: './img/mercenary.jpg',
+        img: 'img/mercenary.jpg',
         text: 'Become a cyberpunk, an urban mercenary equipped with cybernetic enhancements and build your legend on the streets of Night City.'
     },
     {
         heading: 'Implants',
-        img: './img/implant.jpg',
+        img: 'img/implant.jpg',
         text: 'Take the riskiest job of your life and go after a prototype implant that is the key to immortality.'
     }
 ]
@@ -43,9 +43,9 @@ nav.appendChild(navList);
 const main = document.createElement('main');
 main.classList.add('main');
 
-// Items generation
+// Items 
 items.forEach( item => {
-    const navItem = createTextElementWithClassName('li', item.heading, 'nav__item');
+    const navItem = createElementWithClassName('li', item.heading, 'nav__item');
     const mainItem = createItem(item);
     navList.appendChild(navItem);
     main.appendChild(mainItem);
@@ -61,21 +61,46 @@ p.insertAdjacentHTML('afterbegin', `Created by <a href="https://github.com/serhi
 footer.appendChild(p);
 body.appendChild(footer);
 
-
 //Functions 
-function createTextElementWithClassName(tag, text, className) {
+function createElementWithClassName(tag, value, className) {
     const element = document.createElement(tag);
     element.classList.add(className);
-    element.textContent = text;
+    switch(tag){
+        case 'img':
+            element.src = value;
+            element.alt = value.replace('img/', '').replace('.jpg','');
+            break;
+        default:
+            element.textContent = value;
+            break;
+    }
     return element;
 }
 
 function createItem(item) {
     const div = document.createElement('div');
     div.classList.add('main__item', item.heading.toLowerCase().split(' ').join('_'));
-    const mainItemContent = createTextElementWithClassName('p', item.text, 'main__content');
+    const mainItemContent = createElementWithClassName('p', item.text, 'main__content');
     div.appendChild(mainItemContent);
+    const mainItemImage = createElementWithClassName('img', item.img, 'main__image');
+    div.appendChild(mainItemImage);
+    div.classList.add('hidden');
     return div;
 }
 
+// Hide and unhide items
+const navSection = document.getElementsByClassName('nav__item');
+for (var i = 0; i < navSection.length; i++) {
+    navSection[i].addEventListener('click', function () {
+        hideItems(); 
+        document.getElementsByClassName(this.textContent.toLowerCase().split(' ').join('_'))[0].classList.remove('hidden');
+    });
+}
 
+function hideItems(){
+    let arrayOfElements = Array.from(document.getElementsByClassName('main__item'));
+    arrayOfElements.map( element => {
+        if (!element.classList.contains('hidden')) element.classList.add('hidden');
+    } 
+    );
+}
